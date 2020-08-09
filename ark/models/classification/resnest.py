cc9b8fca61ee57983db36aeb08e6a173217a741e
bernardomig <bernardo.lourenco@ua.pt>
2020-08-10 00:46:55 +0100

    added first draft of the resnest model

---
+++ ark/models/classification/resnest.py
@@ -0,0 +1,159 @@
+from typing import List, Tuple
+from functools import partial
+import torch
+from torch import nn
+from torch.nn import functional as F
+from ark.nn.easy import ConvBnReLU2d, ConvBn2d
+from .resnet import ResNet
+
+
+def resnest50(in_channels, num_classes):
+    return ResNest(in_channels, num_classes,
+                   block_depth=[3, 4, 6, 3],
+                   init_channels=64,
+                   block_channels=[256, 512, 1024, 2048],
+                   expansion=4,
+                   radix=2,
+                   )
+
+
+def resnest101(in_channels, num_classes):
+    return ResNest(in_channels, num_classes,
+                   block_depth=[3, 4, 23, 3],
+                   init_channels=128,
+                   block_channels=[256, 512, 1024, 2048],
+                   expansion=4,
+                   radix=2,
+                   )
+
+
+def resnest200(in_channels, num_classes):
+    return ResNest(in_channels, num_classes,
+                   block_depth=[3, 24, 36, 3],
+                   init_channels=128,
+                   block_channels=[256, 512, 1024, 2048],
+                   expansion=4,
+                   radix=2,
+                   )
+
+
+def resnest269(in_channels, num_classes):
+    return ResNest(in_channels, num_classes,
+                   block_depth=[3, 30, 48, 8],
+                   init_channels=128,
+                   block_channels=[256, 512, 1024, 2048],
+                   expansion=4,
+                   radix=2,
+                   )
+
+
+class ResNest(ResNet):
+    def __init__(self, in_channels: int, num_classes: int,
+                 block_depth: Tuple[int, int, int, int],
+                 init_channels: int,
+                 block_channels: Tuple[int, int, int, int],
+                 expansion: int = 4,
+                 base_width: int = 64,
+                 cardinality: int = 1,
+                 radix: int = 1):
+
+        block = partial(Bottleneck,
+                        expansion=expansion,
+                        base_width=base_width,
+                        cardinality=cardinality,
+                        radix=radix)
+
+        super(ResNest, self).__init__(
+            in_channels, num_classes,
+            block=block,
+            block_depth=block_depth,
+            init_channels=init_channels,
+            block_channels=block_channels)
+
+        # only change the stem to match the ResNest architecture
+        self.features.stem = nn.Sequential(
+            ConvBnReLU2d(in_channels, init_channels // 2, 3, padding=1, stride=2),
+            ConvBnReLU2d(init_channels // 2, init_channels // 2, 3, padding=1),
+            ConvBnReLU2d(init_channels // 2, init_channels, 3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int,
+                 stride: int = 1,
+                 expansion: int = 4,
+                 base_width: int = 64,
+                 cardinality: int = 1,
+                 radix: int = 1):
+        super(Bottleneck, self).__init__()
+
+        self.radix = radix
+
+        width = int((out_channels / expansion) * (base_width / 64) * cardinality)
+
+        self.conv1 = ConvBnReLU2d(in_channels, width, 1)
+
+        self.conv2 = ConvBnReLU2d(width, width * radix, 3, padding=1, groups=cardinality * radix)
+        self.sa = SplitAttention(width, width * radix,
+                                 4,
+                                 cardinality=cardinality)
+        self.pool = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1) if stride == 2 else nn.Identity()
+        self.conv3 = ConvBn2d(width, out_channels, 1)
+
+        self.activation = nn.ReLU(inplace=True)
+
+        self.downsample = (
+            nn.Sequential(
+                nn.AvgPool2d(stride) if stride != 1 else nn.Identity(),
+                ConvBn2d(in_channels, out_channels, 1),
+            )
+            if stride != 1 or in_channels != out_channels
+            else nn.Identity()
+        )
+
+    def forward(self, input):
+        x = self.conv1(input)
+        x = self.conv2(x)
+        xs = torch.chunk(x, self.radix, dim=1)
+        x = self.sa(xs)
+        x = self.pool(x)
+        x = self.conv3(x)
+        residual = self.downsample(input)
+        return self.activation(x + residual)
+
+
+class SplitAttention(nn.Module):
+    def __init__(self, in_channels, out_channels, reduction, cardinality):
+        super(SplitAttention, self).__init__()
+
+        self.cardinality = cardinality
+
+        mid_channels = out_channels // reduction
+
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, 1, groups=cardinality)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(mid_channels, out_channels, 1, groups=cardinality)
+
+    def forward(self, input: List[torch.Tensor]) -> torch.Tensor:
+        radix = len(input)
+        x = sum(input)
+        at = F.adaptive_avg_pool2d(x, 1)
+        at = self.conv1(at)
+        at = self.bn1(at)
+        at = self.relu(at)
+        at = self.conv2(at)
+        at = radix_softmax(at, radix, self.cardinality)
+
+        ats = torch.split(at, x.size(1), dim=1)
+
+        return sum((at * x for at, x in zip(ats, input)))
+
+
+@torch.jit.script
+def radix_softmax(input: torch.Tensor, radix: int, cardinality: int):
+    batch = input.size(0)
+    x = input.view(batch, cardinality, radix, -1).transpose(1, 2)
+    x = torch.softmax(x, dim=1)
+    return x.reshape_as(input)
