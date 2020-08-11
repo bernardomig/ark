9ed4fe39377ec57cd3c9bd2fae5965c3979be371
bernardomig <bernardo.lourenco@ua.pt>
2020-08-11 19:39:08 +0100

    updated hubconf

--- hubconf.py
+++ hubconf.py
@@ -5,16 +5,21 @@ from ark.models.classification.drn import (
     drn_c_26, drn_c_42, drn_c_58,
     drn_d_22, drn_d_38, drn_d_54, drn_d_105)
 from ark.models.classification.ghostnet import ghostnet_1_0
+from ark.models.classification.mixnet import (
+    mixnet_s, mixnet_m, mixnet_l)
 from ark.models.classification.mobilenetv2 import (
     mobilenetv2_1_0, mobilenetv2_0_75, mobilenetv2_0_50,
     mobilenetv2_0_35, mobilenetv2_0_25, mobilenetv2_0_10)
+from ark.models.classification.mobilenetv3 import (
+    mobilenetv3_large_1_0, mobilenetv3_large_0_75,
+    mobilenetv3_small_1_0, mobilenetv3_small_0_75)
 from ark.models.classification.regnet import (
     regnetx_002, regnetx_004, regnetx_006, regnetx_008,
     regnetx_016, regnetx_032, regnetx_040, regnetx_064,
-    regnetx_120, regnetx_160, regnetx_320,
+    regnetx_080, regnetx_120, regnetx_160, regnetx_320,
     regnety_002, regnety_004, regnety_006, regnety_008,
     regnety_016, regnety_032, regnety_040, regnety_064,
-    regnety_120, regnety_160, regnety_320)
+    regnety_080, regnety_120, regnety_160, regnety_320)
 from ark.models.classification.resnest import (
     resnest50, resnest101, resnest200, resnest269)
 from ark.models.classification.resnet import (
