cc41bc1d2418f84fe0ba04fc8cf626ed509b5f9f
bernardomig <bernardo.lourenco@ua.pt>
2020-08-11 19:17:13 +0100

    fixed bug in resnests

--- ark/models/classification/resnest.py
+++ ark/models/classification/resnest.py
@@ -156,17 +156,17 @@ class SplitAttention(nn.Module):
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         xs = torch.chunk(input, self.radix, dim=1)
-        x = sum(input)
+        x = sum(xs)
         at = F.adaptive_avg_pool2d(x, 1)
         at = self.conv1(at)
         at = self.bn1(at)
         at = self.relu(at)
         at = self.conv2(at)
-        at = radix_softmax(at, radix, self.cardinality)
+        at = radix_softmax(at, self.radix, self.cardinality)
 
         ats = torch.split(at, x.size(1), dim=1)
 
-        return sum((at * x for at, x in zip(ats, input)))
+        return sum((at * x for at, x in zip(ats, xs)))
 
 
 @torch.jit.script
@@ -175,3 +175,9 @@ def radix_softmax(input: torch.Tensor, radix: int, cardinality: int):
     x = input.view(batch, cardinality, radix, -1).transpose(1, 2)
     x = torch.softmax(x, dim=1)
     return x.reshape_as(input)
+
+
+if __name__ == "__main__":
+    x = torch.randn((3, 3, 128, 128))
+    model = resnest50(3, 1000)
+    y = model(x)
